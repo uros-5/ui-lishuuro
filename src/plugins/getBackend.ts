fe90142904b1b8a208778dbeb3db818eb6bba124
uros-5 <uros.mrkobrada99@gmail.com>
2022-05-20 17:47:06 +0200

    updated getBackend

--- src/plugins/getBackend.ts
+++ src/plugins/getBackend.ts
@@ -1,12 +1,13 @@
 export function backend(): string {
     let prod = import.meta.env.PROD;
-    return (prod ? "https://lishuuro.org:8000/" : "http://localhost:8080/");
+    return (prod ? "https://lishuuro.org/w/" : "http://localhost:8080/");
 }
 
 export function wsUrl(): string {
     let prod = import.meta.env.PROD;
-    let h = prod ? "https" : "http"; 
+    let ws = prod ? "wss" : "ws";
+    let h = prod ? "https" : "http";
     let b = (backend() as string).toString();
     let s = b.split(`${h}://`)[1];
-    return `ws://${s}ws/`;
+    return `${ws}://${s}ws/`;
 }
