5e804cad972bec4cf3b902fb1c02606b3c3ea3fa
uros <uros.mrkobrada99@gmail.com>
2023-02-18 01:29:50 +0100

    added new eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,23 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: "latest",
+  },
+  rules : {
+    "vue/no-use-v-if-with-v-for": ["error", {
+        "allowUsingIterationVar": false
+      }],
+    "no-case-declarations": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "vue/require-v-for-key": "off"
   }
-}
+};
