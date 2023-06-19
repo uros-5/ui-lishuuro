bdd16b076c8908987d74af99953b702e49aeb0f6
uros <uros.mrkobrada99@gmail.com>
2023-06-19 22:27:44 +0200

    added buy in audio.ts

--- utils/audio.ts
+++ utils/audio.ts
@@ -2,6 +2,7 @@ import captureUrl from "assets/sounds/capture.ogg";
 import resUrl from "assets/sounds/res.ogg";
 import moveUrl from "assets/sounds/move.ogg";
 import lowTimeUrl from "assets/sounds/low_time.ogg";
+import buy from "assets/sounds/buy.ogg"
 
 export function playAudio(sound: string, volume?: string) {
   let audio;
@@ -17,6 +18,9 @@ export function playAudio(sound: string, volume?: string) {
       break;
     case "low_time":
       audio = lowTimeUrl;
+      break;
+    case "buy":
+      audio = buy;
       break;
   }
   const a = new Audio(audio);
