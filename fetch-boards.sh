0342c61d0b078e7813e3913ee058141ea173c89b
Uros Mrkobrada <uros.mrkobrada@gmail.com>
2025-05-12 23:55:07 +0200

    remove unused components

---
+++ fetch-boards.sh
@@ -0,0 +1,24 @@
+
+mkdir src/assets/boards-live 
+curl https://i.postimg.cc/bYWbcGv8/empty-placement.png --output src/assets/boards-live/empty-placement.png
+curl https://i.postimg.cc/Qx77XXB7/empty-placement-shuuro.png --output src/assets/boards-live/empty-placement-shuuro.png
+curl https://i.postimg.cc/prwnDSSy/howto2.png --output src/assets/boards-live/howto2.png
+curl https://i.postimg.cc/XqGCBWQT/howto3.png --output src/assets/boards-live/howto3.png
+curl https://i.postimg.cc/bJFnvrHx/howto4.png --output src/assets/boards-live/howto4.png
+curl https://i.postimg.cc/W4TgWQ1k/intro-meme.png --output src/assets/boards-live/intro-meme.png
+curl https://i.postimg.cc/3Rwp7mpr/live-standard.png --output src/assets/boards-live/live-standard.png
+curl https://i.postimg.cc/ZqH31VWD/mini.png --output src/assets/boards-live/mini.png
+curl https://i.postimg.cc/Cxjb4FS0/mini-Fairy.png --output src/assets/boards-live/mini-Fairy.png
+curl https://i.postimg.cc/ZKv8C8QC/placement-standard.png --output src/assets/boards-live/placement-standard.png
+curl https://i.postimg.cc/rw6SWd6g/placement-standard-2.png --output src/assets/boards-live/placement-standard-2.png
+curl https://i.postimg.cc/PqjQGC2N/selection.png --output src/assets/boards-live/selection.png
+curl https://i.postimg.cc/pr4KsrmW/selection-2.png --output src/assets/boards-live/selection-2.png
+curl https://i.postimg.cc/xj4Gfxtw/selection-extended.png --output src/assets/boards-live/selection-extended.png
+curl https://i.postimg.cc/jj4QRRFc/selection-fairy.png --output src/assets/boards-live/selection-fairy.png
+curl https://i.postimg.cc/2yV4dh89/selection-fairy-2.png --output src/assets/boards-live/selection-fairy-2.png
+curl https://i.postimg.cc/FRFbR2t4/selection-standard.png --output src/assets/boards-live/selection-standard.png
+curl https://i.postimg.cc/qMNL2VNB/shuuro-live.png --output src/assets/boards-live/shuuro-live.png
+curl https://i.postimg.cc/50PBFn1F/shuuro-live-fairy.png --output src/assets/boards-live/shuuro-live-fairy.png
+curl https://i.postimg.cc/bYCgGNNF/standard.png --output src/assets/boards-live/standard.png
+curl https://i.postimg.cc/MKgYDPB7/standard-fairy-live.png --output src/assets/boards-live/standard-fairy-live.png
+curl https://i.postimg.cc/NjTkP68W/standard-live.png --output src/assets/boards-live/standard-live.png
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/components/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
